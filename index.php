619253f507f7edd8c608131c04fb358f8c203bb0
root <root@Durbin.Durbin>
2020-11-10 04:03:36 +0600

    comite.....v2

--- index.php
+++ index.php
@@ -38,47 +38,13 @@
   </head>
 
     <!-- navbar header start -->
-  <header>
-
-      <nav class="navbar navbar-expand-lg navbar-light custom_White">
-        <div class="navbar sky_blue">
-          <img class="center" src="image/logo_JPF.png" width="80" height="40" alt="" loading="lazy">
-
-        </div>
-        <!-- <a class="navbar-brand" href="#">Navbar</a> -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-              <a class="nav-link text-info text-uppercase " href="#">Dashboard <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text-uppercase" href="#">Stock</a>
-            </li>
-            <li class="nav-item dropdown text-uppercase">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Depo
-              </a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Sales Report</a>
-                <a class="dropdown-item" href="#">Invoice</a>
-                <!-- <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a> -->
-              </div>
-            </li>
-
-          </ul>
-
-        </div>
-    </nav>
-  </header>
+    <?php
+    include 'header.php';
+    ?>
   <!-- navbar header End -->
 
   <body >
+
     <!-- Fixed sidebar start -->
     <div class="sidebar">
 
